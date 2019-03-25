04bfa590c267669049dd2a1416235b84e601aa44
Chauncy <35761045+SicParvisMagna95@users.noreply.github.com>
2019-03-25 19:49:49 +0800

    0325

---
+++ simplenet/dataset.py
@@ -0,0 +1,24 @@
+import torch
+import torch.utils.data as Data
+import torchvision.transforms as transforms
+
+
+
+class Mydataset(Data):
+    def __init__(self):
+        super(Mydataset,self).__init__()
+
+
+
+    def __len__(self):
+        pass
+
+
+
+    def __getitem__(self, item):
+        pass
+
+
+
+
+
